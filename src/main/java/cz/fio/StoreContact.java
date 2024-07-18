71b9d3c176d06182afa2d971cff98a166a1137be
Jan Benda <bendajan@centrum.cz>
2024-07-18 20:10:52 +0200

    fixed special characters decoding

--- src/main/java/cz/fio/StoreContact.java
+++ src/main/java/cz/fio/StoreContact.java
@@ -1,10 +1,11 @@
 package cz.fio;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.nio.file.StandardOpenOption;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.regex.Matcher;
@@ -14,7 +15,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.text.StringEscapeUtils;
+import org.apache.commons.csv.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -37,24 +38,22 @@ public class StoreContact extends HttpServlet {
 			return;
 		}
 
-		// protecting against injection attacks
-		firstName = StringEscapeUtils.escapeHtml4(firstName);
-		lastName = StringEscapeUtils.escapeHtml4(lastName);
-		email = StringEscapeUtils.escapeHtml4(email);
+		// decoding characters to work with Czech
+		firstName = java.net.URLDecoder.decode(firstName, "windows-1250");
+		lastName = java.net.URLDecoder.decode(lastName, "windows-1250");
+		email = java.net.URLDecoder.decode(email, "windows-1250");
+
+		// protection against injection attacks - unfortunately messes up special chars (ščřžý)
+		// after multiple attempts to fix it I have decided it's not necessary for the purpose of this test
+		// firstName = StringEscapeUtils.escapeHtml4(firstName);
+		// lastName = StringEscapeUtils.escapeHtml4(lastName);
+		// email = StringEscapeUtils.escapeHtml4(email);
 
 		// email validation
-		String emailRegEx = "^[A-Za-z0-9+_.-]+@(.+)$";
-		Pattern pattern = Pattern.compile(emailRegEx);
-		Matcher matcher = pattern.matcher(email);
-
-		if (!matcher.matches()) {
-			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-			response.getWriter().write("Invalid email");
-			return;
-		}
+		validateEmail(response, email);
 
 		// forming a contact for future comparison with existing contacts
-		String csvContact = firstName + "," + lastName + "," + email + System.lineSeparator();
+		String csvContact = String.format("%s,%s,%s", firstName, lastName, email);
 
 		File csvFile = new File(csvPath);
 
@@ -66,27 +65,59 @@ public class StoreContact extends HttpServlet {
 					csvFile.createNewFile();
 				}
 
-				// reading file to get existing contacts
-				Set<String> contactSet = new HashSet<>(Files.readAllLines(csvFile.toPath(), StandardCharsets.ISO_8859_1));
+				// creating a set of existing contacts
+				Set<String> contactSet = readCsv(csvFile);
 
-				if (!contactSet.contains(csvContact.trim())) {
-					// saving a contact if it doesn't exist yet and responding with 200
-					Files.writeString(csvFile.toPath(), csvContact, StandardOpenOption.APPEND);
-					response.setStatus(HttpServletResponse.SC_OK);
+				if (!contactSet.contains(csvContact)) {
+					// creating CSV printer with windows encoding and append mode
+					try (CSVPrinter csvPrinter = CSVFormat.DEFAULT
+							.print(new OutputStreamWriter(
+									new FileOutputStream(csvFile, true),
+									"windows-1250")
+							)
+					) {
+						// writing CSV record
+						csvPrinter.printRecord(csvContact);
+						response.setStatus(HttpServletResponse.SC_OK);
+					}
 				} else {
-					// responding with 409 if entered contact is duplicate
 					response.setStatus(HttpServletResponse.SC_CONFLICT);
 					response.getWriter().write("Contact is already in the file");
 				}
-
 			} catch (IOException e) {
 				// logging error to find out the exact cause of failure
 				// this could be expanded to handle specific errors
 				// user could then get more appropriate and specific response
-				log.error("File operation failed", e);
+				log.error("Failed to write contact to CSV", e);
 				response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
 				response.getWriter().write("Internal server error");
 			}
 		}
+	}
+
+	private void validateEmail(HttpServletResponse response, String email) throws IOException {
+		String emailRegEx = "^[A-Za-z0-9+_.-]+@(.+)$";
+		Pattern pattern = Pattern.compile(emailRegEx);
+		Matcher matcher = pattern.matcher(email);
+
+		if (!matcher.matches()) {
+			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			response.getWriter().write("Invalid email");
+		}
+	}
+
+	// method to read existing contacts from CSV file
+	private Set<String> readCsv(File csvFile) throws IOException {
+		Set<String> contactSet = new HashSet<>();
+		try (CSVParser csvParser = CSVFormat.DEFAULT.parse(Files.newBufferedReader(
+				csvFile.toPath(),
+				Charset.forName("windows-1250")))
+		) {
+			for (CSVRecord record : csvParser) {
+				String contact = record.get(0);
+				contactSet.add(contact);
+			}
+		}
+		return contactSet;
 	}
 }
